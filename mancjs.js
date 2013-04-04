e15a67f9bc097d34cf65338d9bb00694c9d28966
Ryan Gough <ryan.gough@gmail.com>
2013-04-04 23:11:18 +0100

    make it actually do something

--- mancjs.js
+++ mancjs.js
@@ -1,4 +1,12 @@
+var request = require('request');
+var cheerio = require('cheerio');
+
 module.exports.getNextDate = function getNextDate(callback){
-	callback(null, "03 Apr 19:00");
+request('http://mancjs.com', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+  	var $ = cheerio.load(body);
+    callback(null, $(".badge")[0].children[0].data);
+  }
+})
 }
 
